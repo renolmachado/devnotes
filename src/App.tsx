ef6b2eab8704449c18d0486e8517ed74b7eecb38
Renol Machado <renol94@gmail.com>
2022-08-26 15:08:59 -0300

    Create Tauri + Vite + React app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,50 @@
-import logo from './logo.svg'
+import { useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
 import './App.css'
+import reactLogo from './assets/react.svg'
 
 function App() {
+  const [greetMsg, setGreetMsg] = useState('')
+  const [name, setName] = useState('')
+
+  async function greet() {
+    setGreetMsg(await invoke('greet', { name }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <h1>Welcome to Tauri! Renol Machado</h1>
+
+      <div className="row">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
         </a>
-      </header>
+        <a href="https://tauri.app" target="_blank" rel="noreferrer">
+          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <p>
+        Click on the Tauri, Vite, and React logos to learn more about each
+        framework.
+      </p>
+
+      <div className="row">
+        <div>
+          <input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+          <button type="button" onClick={() => greet()}>
+            Greet
+          </button>
+        </div>
+      </div>
+      <p>{greetMsg}</p>
     </div>
   )
 }
